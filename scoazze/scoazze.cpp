65952115b865f23d1c597188f006878998e5d07e
ChampaXIII <101710151+ChampaXIII@users.noreply.github.com>
2022-12-13 16:18:51 +0100

    Add files via upload

--- scoazze/scoazze.cpp
+++ scoazze/scoazze.cpp
@@ -7,13 +7,13 @@
 using namespace std;
 
 // input data
-int N, K;
-vector<int> C, T, Q;
+long long N, K;
+vector<long long> C, T, Q;
 
-vector<int> Cbin;
-int prezzo = 0;
+vector<long long> Cbin;
+long long prezzo = 0;
 
-int main() {
+long long main() {
 //  uncomment the following lines if you want to read/write from files
   ifstream cin("input.txt");
 //  ofstream cout("output.txt");
@@ -21,18 +21,18 @@ int main() {
     cin >> N >> K;
     C.resize(N);
     Cbin.resize(N);
-    for (int i=0; i<N; i++){
+    for (long long i=0; i<N; i++){
         cin >> C[i];
         Cbin[i] = 0;
     }
 
     T.resize(K);
     Q.resize(K);
-    for (int i=0; i<N; i++)
+    for (long long i=0; i<N; i++)
         cin >> T[i] >> Q[i];
     // insert your code here
 
-    for(int i = 0; i < K; i++){
+    for(long long i = 0; i < K; i++){
         if(Cbin[T[i]] == C[T[i]]){
             Cbin[T[i]] = 0;
         }else{
@@ -44,16 +44,17 @@ int main() {
             }
         }
         if(i != K-1){
-            for(int n = 0; n < N; N++){
+            for(long long n = 0; n < N; N++){
                 if(Cbin[n] != 0){
                     prezzo += C[n] - Cbin[n];
                 }
             }
         }
+        cout << i << ": " << prezzo << endl;
     }
 
 
-    cout << prezzo << endl; // print the result
+    cout << prezzo << endl; // prlong long the result
     return 0;
 }
 */
@@ -64,13 +65,13 @@ int main() {
 using namespace std;
 
 // input data
-int N, K;
-vector<int> C, T, Q;
+long long N, K;
+vector<long long> C, T, Q;
 
-vector<int> Cbin;
-int prezzo = 0;
+vector<long long> Cbin;
+long long prezzo = 0;
 
-int main() {
+long long main() {
 //  uncomment the following lines if you want to read/write from files
 //  ifstream cin("input.txt");
 //  ofstream cout("output.txt");
@@ -80,37 +81,39 @@ int main() {
 
     Cbin.resize(N);
 
-    for (int i=0; i<N; i++){
+    for (long long i=0; i<N; i++){
         cin >> C[i];
         Cbin[i] = 0;
     }
 
     T.resize(K);
     Q.resize(K);
-    for (int i=0; i<K; i++)
+    for (long long i=0; i<K; i++)
         cin >> T[i] >> Q[i];
     // insert your code here
 
-    for(int i = 0; i < K; i++){
+    for(long long i = 0; i < K; i++){
         if(Cbin[T[i]] == C[T[i]]){
             Cbin[T[i]] = 0;
         }else{
             if(Cbin[T[i]] + Q[i] > C[T[i]]){
                 prezzo += C[T[i]] - Cbin[T[i]];
-                Cbin[T[i]] = 0;
+                Cbin[T[i]] = Q[i];
+
             }else{
                 Cbin[T[i]] += Q[i];
             }
         }
         if(i != K-1){
-            for(int n = 0; n < N; N++){
+            for(long long n = 0; n < N; n++){
                 if(Cbin[n] != 0){
                     prezzo += C[n] - Cbin[n];
                 }
             }
         }
+        // << i << ": " << prezzo << endl;
     }
 
-    cout << prezzo << endl; // print the result
+    cout << prezzo << endl; // prlong long the result
     return 0;
 }
